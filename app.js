74e282046b8e978088853b4081d936ec7a76db85
Musadiqaman <musadiqaman2006@gmail.com>
2025-12-05 01:28:33 -0800

    all files added

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(
 // 🛡 SECURITY LAYER 3 → CORS (Local + Vercel ready)
 // =======================================================
 const allowedOrigins = process.env.NODE_ENV === "production"
-  ? ["https://paintsstore.vercel.app"]  
+  ? ["fuck"]  
   : ["http://localhost:3000"];
 
 app.use(cors({
